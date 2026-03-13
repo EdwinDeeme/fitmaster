196b7f625c93afedf7c597a0bb737fdca450553f
EdwinDeeme <edwingyt@gmail.com>
2026-03-13 11:52:34 -0600

    Scrollbar Ajustes

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang="es" className="overflow-y-scroll">
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
